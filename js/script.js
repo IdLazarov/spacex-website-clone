19941d685e44b7676e9629acecea79c791628aba
Ivan Lazarov <ivan@betixon.com>
2022-08-17 18:03:02 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
+
+btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage);
+
+function navToggle(){
+    btn.classList.toggle("open");
+    overlay.classList.toggle("overlay-show");
+    document.body.classList.toggle("stop-scrolling");
+    menu.classList.toggle("show-menu");
+}
+
+
+function scrollPage(){
+    //Scroll position
+    const scrollPos = window.scrollY;
+
+    if(scrollPos > 100 && !scrollStarted){
+        countUp();
+        scrollStarted = true
+    }
+    else if(scrollPos < 100 && scrollStarted){
+        reset();
+        scrollStarted = false;
+    }
+    
+}
+
+function countUp(){
+    counters.forEach((counter)=>{
+        
+        counter.innerText = "0";
+        const updateCounter = () =>{
+            //+ sign turns the string to a number
+
+            //Get target counter value
+            const target = +counter.getAttribute("data-target");
+            
+
+            //Get current counter value
+            const c = +counter.innerText;
+
+            //Create an increment
+            const increment = target / 100;
+
+            //If counter is less than the target add increment
+            if(c < target){
+                //Round up and set counter value
+                counter.innerText = `${Math.ceil(c + increment)}`;
+                setTimeout(updateCounter, 75);
+            } 
+            else{
+                counter.innerText = target;
+            }
+            
+        }
+
+        updateCounter();
+    })
+}
+
+function reset(){
+    counters.forEach((counter) => counter.innerHTML = "0");
+}
+
+countUp();
